aa00767ffd4e9723bc965ef1471d6040925d8320
Pavlos Andrade <pavlos.andrade@novasingular.com.br>
2022-07-26 12:36:19 -0300

    troca do carrousel do blazor pelo tiny slider

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -5,3 +5,20 @@
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+
+
+function InitBannerSliderJs(bannerSlider, controlsSlider) {
+    var slider = tns({
+        container: bannerSlider,
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        autoplayHoverPause: true,
+        autoplayButtonOutput: false,
+        controls: true,
+        controlsContainer: controlsSlider,
+        mouseDrag: true,
+        navPosition: "bottom",
+    });
+}
